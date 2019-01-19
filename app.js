e51394af4fae87baa84aefa9aa913e9a06cbaae1
Malazkoj <malazqog@gmail.com>
2019-01-19 14:42:34 +0100

    trying to test treevie with dummy info, we will try to fill it from JS later

--- app.js
+++ app.js
@@ -40,3 +40,32 @@ app.listen(port, () => console.log('Starting web application on port 3000...'));
 
 // Required by ESM (ES6)
 export {}
+// main(){
+     const categoryArray =createCategoryTreeNO();
+// }
+// create the category tree in norwegian
+function createCategoryTreeNO() {
+    //categoryfather is 0 if it's a general/hoved category, or categoryFather is its category's father and it's a sub category
+    //let category = {categoryID:0,categoryFatherID:"",categoryName:"",categoryDescription:"",categoryTitle:""};
+    let category1= createCategory("1.0","0","Jobb","Lurer du på noe gelder jobb","");
+    let category2= createCategory("2.0","0","Lover og regler","Lurer du på noe gelder Lover of regler","");
+    let category3= createCategory("3.0","0","Aktiviteter","Lurer du p[ noe gelder jobb","");
+    let category4= createCategory("4.0","0","NorskOpplæring og utdanning ","Lurer du p[ noe gelder opplæring","");
+    let category5= createCategory("5.0","0","Viktige linker ","Lurer du p[ noe gelder jobb","");
+const categoryArray=[category1,category2,category3,category4,category5];
+return categoryArray;
+    //const categoryArray=[
+    //    category.categoryID=1.
+    //]
+
+}
+function createCategory(categoryID,categoryFatherID,categoryName,categoryDescription,categoryTitle){
+    let myCategory={};
+    myCategory.categoryID=categoryID;
+    myCategory.categoryName=categoryFatherID;
+    myCategory.categoryName=categoryName;
+    myCategory.categoryDescription=categoryDescription;
+    myCategory.categoryTitle=categoryTitle;
+    return myCategory;
+
+}
