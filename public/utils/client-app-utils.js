b17e0734b7200c320d80fe497bedc5d546f97435
Haimanot1989 <haimif@yahoo.com>
2019-02-02 22:18:38 +0100

    add endpoint to fetch titles of interest from our hardcoded data, so that it can populate the drop down list

--- public/utils/client-app-utils.js
+++
@@ -1,9 +0,0 @@
-export function greetings(name) {
-    return `Hello ${name}!`;
-}
-
-export function sayHello(name) {
-    console.log("Say Hello to " + name);
-    document.getElementById('say-hello').innerHTML=greetings(name);
-    return true;
-}
