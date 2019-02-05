b3f3b612c94b59340317063e421c8a543df3c0ed
Haimanot1989 <haimif@yahoo.com>
2019-02-05 23:49:07 +0100

    populated interest dropdown list dynamically

--- public/client-app.js
+++ public/client-app.js
@@ -4,13 +4,30 @@
  */
 
 function main() {
-    const userAction = async () => {
-        const response = await fetch('http://localhost:3000/api/categories');
-        const departure = await response.json(); //extract JSON from the http response
-    }
+    let interestTitles = [];
+
+    const response = fetch('/api/interest-titles')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            interestTitles = Object.values(myJson);
+            populateInterestDropDownList(interestTitles);
+        });
 
 }
 
+function populateInterestDropDownList(interestTitles){
+
+    var dropdown = document.getElementById("inputGroupSelect02");
+
+    // Loop through the array
+    for (var i = 0; i < interestTitles.length; ++i) {
+        // Append the element to the end of Array list
+        dropdown[dropdown.length] = new Option(interestTitles[i], interestTitles[i]);
+    }
+
+}
 
 // Calling main on load
 window.onload = main;
